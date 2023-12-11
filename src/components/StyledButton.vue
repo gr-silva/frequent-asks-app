901e6b9b6b87d30b32eb095a5c0f5f1fde685aa4
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-11 17:42:04 -0300

    fix: change component name

--- src/components/StyledButton.vue
+++ src/components/StyledButton.vue
@@ -1,37 +1,38 @@
 <template>
-  <button class="button-with-slots">
+  <button class="styled-button">
     <!--FIXME: CORRIGIR BUG TAMANHO SLOT  -->
-    <slot class="slot-icon" />
-    <p class="text-of-buttons-with-slots">{{ text }}</p>
+    <div class="w-full">
+      <slot v-if="hasSlot" class="slot-icon" />
+      <span class="text-of-styled-button">{{ text }}</span>
+    </div>
   </button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({ props: { text: String } })
+export default defineComponent({ props: { text: String, hasSlot: Boolean } })
 </script>
 
 <style scoped>
-.button-with-slots {
+.styled-button {
   margin-left: 5px;
   margin-right: 6px;
   margin-top: 32px;
+
   top: 541px;
   left: 701px;
   width: 293px;
   height: 53px;
 
+  border: none;
+  background: none;
+}
+.styled-button:hover {
   background: #3f4452b3 0% 0% no-repeat padding-box;
   opacity: 1;
 }
-
 .slot-icon {
-  margin-left: 32.35px;
-  margin-right: 239.73px;
-  margin-top: 17px;
-  margin-bottom: 15.29px;
-
   top: 609px;
   left: 732px;
   width: 18px;
@@ -42,18 +43,17 @@ export default defineComponent({ props: { text: String } })
   opacity: 1;
 }
 
-.text-of-buttons-with-slots {
+.text-of-styled-button {
   top: 611px;
   left: 786px;
   width: 72px;
   height: 19px;
 
-  margin-top: 17px;
-  margin-bottom: 17px;
-  margin-left: 85px;
-  margin-right: 137;
+  padding-top: 17px;
+  padding-bottom: 17px;
+  padding-left: 85px;
+  padding-right: 137px;
 
-  color: var(--unnamed-color-f5f6f8);
   text-align: left;
   font: normal normal normal 16px/19px Lato;
   letter-spacing: 0px;
